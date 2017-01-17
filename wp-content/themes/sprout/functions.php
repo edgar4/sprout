7cba283694785df89924d350298a3a163f8b3957
edgar chris <edgarchris4@gmail.com>
2017-01-17 17:46:24 +0300

    set heeaders

--- wp-content/themes/sprout/functions.php
+++ wp-content/themes/sprout/functions.php
@@ -1,5 +1,22 @@
 <?php
 
+// Allow from any origin
+if (isset($_SERVER['HTTP_ORIGIN'])) {
+    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+    header('Access-Control-Allow-Credentials: true');
+    header('Access-Control-Max-Age: 86400');    // cache for 1 day
+}
+// Access-Control headers are received during OPTIONS requests
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+        header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+        header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+
+}
+
 
 add_action('wp_ajax_nopriv_ajax_login', 'ajax_login');
 add_action('wp_ajax_post_love_add_love', 'ajax_login');
