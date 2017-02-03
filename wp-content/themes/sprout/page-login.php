ce593da59be97082cae73b6e2e233c52cbd560d4
edgar chris <edgarchris4@gmail.com>
2017-02-03 17:19:48 +0300

    add events and subscriptions

--- wp-content/themes/sprout/page-login.php
+++ wp-content/themes/sprout/page-login.php
@@ -6,7 +6,8 @@ echo '<pre>';
 //echo  ajax_get_student_activity();
 // echo ajax_get_student_profile();
 
-echo  ajax_get_school_event();
+//echo  ajax_get_school_event();
+echo do_shortcode('[pesapal_pay_payment_form button_name=\'Buy Using Pesapal\' amount=\'10\']');
 exit;
 
 ?>
