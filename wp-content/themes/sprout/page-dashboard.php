92902b983cd7b5532b65719124ec1c90f9c1089f
edgar chris <edgarchris4@gmail.com>
2017-02-10 23:53:48 +0300

    change boostraps

--- wp-content/themes/sprout/page-dashboard.php
+++ wp-content/themes/sprout/page-dashboard.php
@@ -21,26 +21,28 @@ get_header(); ?>
     <div class="container-fluid">
 
 
-
-        <div class="row-fluid offset1">
-            <div class="span12">
+        <div class="row-fluid col-md-offset-1 text-center">
+            <div class="row">
                 <div class="widget-box2 text-center">
 
-                    <div class="span3">
-                        <a href="<?php echo site_url().'/student-activity?activity=1'?>"> <img
-                            src="<?php echo get_stylesheet_directory_uri() . '/assets/images/icon-checkin.png' ?>" class="img-responsive"/>
-                        <h5> Check in</h5>
+                    <div class="col-xs-3 span">
+                        <a href="<?php echo site_url() . '/student-activity?activity=1' ?>"> <img
+                                src="<?php echo get_stylesheet_directory_uri() . '/assets/images/icon-checkin.png' ?>"
+                                class="img-responsive"/>
+                            <h5> Check in</h5>
                         </a>
                     </div>
-                    <div class="span3">
-                        <a href="<?php echo site_url().'/student-activity?activity=2'?>"><img
-                            src="<?php echo get_stylesheet_directory_uri() . '/assets/images/icon-learning.png' ?>" class="img-responsive"/>
-                        <h5> Learning</h5>
+                    <div class="col-xs-3">
+                        <a href="<?php echo site_url() . '/student-activity?activity=2' ?>"><img
+                                src="<?php echo get_stylesheet_directory_uri() . '/assets/images/icon-learning.png' ?>"
+                                class="img-responsive"/>
+                            <h5> Learning</h5>
                         </a>
                     </div>
-                    <div class="span3"><a href="<?php echo site_url().'/student-activity?activity=3'?>"><img
-                            src="<?php echo get_stylesheet_directory_uri() . '/assets/images/icon-food.png' ?>" class="img-responsive"/>
-                        <h5> Food</h5></a>
+                    <div class="col-xs-3"><a href="<?php echo site_url() . '/student-activity?activity=3' ?>"><img
+                                src="<?php echo get_stylesheet_directory_uri() . '/assets/images/icon-food.png' ?>"
+                                class="img-responsive"/>
+                            <h5> Food</h5></a>
                     </div>
 
                 </div>
@@ -51,24 +53,27 @@ get_header(); ?>
         </div>
 
 
-        <div class="row-fluid offset1">
-            <div class="span12">
+        <div class="row-fluid col-md-offset-1">
+            <div class="row">
                 <div class="widget-box2 text-center">
                     &nbsp;<br/> &nbsp;
-            &nbsp;<br/> &nbsp;
-                    <div class="span3">
-                        <a href="<?php echo site_url().'/student-activity?activity=4'?>"> <img
-                            src="<?php echo get_stylesheet_directory_uri() . '/assets/images/icon-note.png' ?>" class="img-responsive"/>
-                        <h5> Note</h5></a>
+                    &nbsp;<br/> &nbsp;
+                    <div class="col-xs-3">
+                        <a href="<?php echo site_url() . '/student-activity?activity=4' ?>"> <img
+                                src="<?php echo get_stylesheet_directory_uri() . '/assets/images/icon-note.png' ?>"
+                                class="img-responsive"/>
+                            <h5> Note</h5></a>
                     </div>
-                    <div class="span3"><a href="<?php echo site_url().'/student-activity?activity=5'?>"><img
-                            src="<?php echo get_stylesheet_directory_uri() . '/assets/images/icon-honours.png' ?>" class="img-responsive"/>
+                    <div class="col-xs-3"><a href="<?php echo site_url() . '/student-activity?activity=5' ?>"><img
+                                src="<?php echo get_stylesheet_directory_uri() . '/assets/images/icon-honours.png' ?>"
+                                class="img-responsive"/>
 
-                        <h5> Honours</h5></a>
+                            <h5> Honours</h5></a>
                     </div>
-                    <div class="span3"><a href="<?php echo site_url().'/student-activity?activity=6'?>"><img
-                            src="<?php echo get_stylesheet_directory_uri() . '/assets/images/icon-potty.png' ?>" class="img-responsive"/>
-                        <h5> Porty</h5></a>
+                    <div class="col-xs-3"><a href="<?php echo site_url() . '/student-activity?activity=6' ?>"><img
+                                src="<?php echo get_stylesheet_directory_uri() . '/assets/images/icon-potty.png' ?>"
+                                class="img-responsive"/>
+                            <h5> Porty</h5></a>
                     </div>
                 </div>
 
@@ -78,29 +83,33 @@ get_header(); ?>
         </div>
 
 
-        <div class="row-fluid offset1">
-            <div class="span12">
+        <div class="row-fluid col-md-offset-1">
+            <div class="row">
                 <div class="widget-box2 text-center">
 
-
-                    <div class="span3"><a href="<?php echo site_url().'/student-activity?activity=7'?>"><img
-                            src="<?php echo get_stylesheet_directory_uri() . '/assets/images/icon-clinic.png' ?>" class="img-responsive"/>
-                        <h5> Clinic</h5></a>
+                    &nbsp;<br/> &nbsp;
+                    &nbsp;<br/> &nbsp;
+                    <div class="col-xs-3"><a href="<?php echo site_url() . '/student-activity?activity=7' ?>"><img
+                                src="<?php echo get_stylesheet_directory_uri() . '/assets/images/icon-clinic.png' ?>"
+                                class="img-responsive"/>
+                            <h5> Clinic</h5></a>
                     </div>
-                    <div class="span3"><a href="<?php echo site_url().'/student-activity?activity=8'?>"><img
-                            src="<?php echo get_stylesheet_directory_uri() . '/assets/images/icon-sleep.png' ?>" class="img-responsive"/>
-                        <h5>Sleep</h5>
+                    <div class="col-xs-3"><a href="<?php echo site_url() . '/student-activity?activity=8' ?>"><img
+                                src="<?php echo get_stylesheet_directory_uri() . '/assets/images/icon-sleep.png' ?>"
+                                class="img-responsive"/>
+                            <h5>Sleep</h5>
                     </div>
-                    <div class="span3"><a href="<?php echo site_url().'/student-activity?activity=9'?>"><img
-                            src="<?php echo get_stylesheet_directory_uri() . '/assets/images/icon-sports.png' ?>" class="img-responsive"/>
-                        <h5> Sports</h5></a>
+                    <div class="col-xs-3"><a href="<?php echo site_url() . '/student-activity?activity=9' ?>"><img
+                                src="<?php echo get_stylesheet_directory_uri() . '/assets/images/icon-sports.png' ?>"
+                                class="img-responsive"/>
+                            <h5> Sports</h5></a>
                     </div>
                 </div>
 
 
             </div>
 
-        </div>
+        </div> <!-- third-->
     </div>
 
 <?php get_footer();
