dae47934f607133464d7301d6ff850ff96811602
Edgar chris <edgarchris4@gmail.com>
2017-05-13 15:09:42 +0300

    permissions

--- wp-config.php
+++ wp-config.php
@@ -22,7 +22,8 @@
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
 define('DB_NAME', 'sproute');
-
+define('WP_HOME','https://www.dashboard.sprout-ke.co.ke/');
+define('WP_SITEURL','https://www.dashboard.sprout-ke.co.ke/');
 /** MySQL database username */
 define('DB_USER', 'root');
 
